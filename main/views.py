fc8540b2107fe85d64f18e74f69e94f0f9b92522
Kirill <randomnik192@gmail.com>
2022-05-23 22:42:40 +0300

    Организована загрузка темплейтов, начата работа со статик файлами

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
+import re
 from django.shortcuts import render
-
+from Tasker.settings import BASE_DIR
 # Create your views here.
+def main_view(request):
+    print(BASE_DIR.parent)
+    return render(request,'index.html')
