fe411e1f64d8a9c3f23c285f155e1e86c27e007f
Kirill <randomnik192@gmail.com>
2022-05-23 23:49:02 +0300

    Организованы модели, убран ненужный templates

--- main/views.py
+++ main/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 from Tasker.settings import BASE_DIR
 # Create your views here.
 def main_view(request):
-    print(BASE_DIR.parent)
     return render(request,'index.html')
